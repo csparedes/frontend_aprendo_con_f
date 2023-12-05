96031ae994ae1345a1728b6c4cd83da9bd95cf91
xavier <xavier.cobos.29@hotmail.com>
2023-12-04 23:42:16 -0500

    Implementación de servicio para cambio de estados alumnos y profesores.

--- AprendoConF/src/app/interfaces/user.interface.ts
+++ AprendoConF/src/app/interfaces/user.interface.ts
@@ -36,26 +36,6 @@ export interface User {
   status: string;
 }
 
-export interface UserPr {
-  id: number;
-  registered: boolean;
-  imageUrl: string;
-  username: string;
-  password: string;
-  email: string;
-  name: string;
-  postalcode: string;
-  country: string;
-  role: string;
-  knowledgeAreas: string[];
-  location: {
-    name: string;
-    country: string;
-    pinImage: string;
-  };
-  price: string;
-  rating: number;
-  phone?: string;
-  description?: string;
-  experience?: number;
+export interface sendStatus {
+  status: string;
 }
