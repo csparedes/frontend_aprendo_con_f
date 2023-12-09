fdeff295b2b8a9ea32aaea6719791f3877758ce2
xavier <xavier.cobos.29@hotmail.com>
2023-12-09 16:22:23 -0500

    Aumento de botón logOut, y elminación de token

--- AprendoConF/src/app/components/app-bar/app-bar.component.ts
+++ AprendoConF/src/app/components/app-bar/app-bar.component.ts
@@ -1,17 +1,26 @@
-import { Component, EventEmitter, ViewChild } from '@angular/core';
+import { Component, EventEmitter, ViewChild, inject } from '@angular/core';
 import { LoginModalComponent } from '../modal/login-modal/login-modal.component';
+import { Router } from '@angular/router';
 declare var bootstrap: any;
 @Component({
   selector: 'app-app-bar',
   templateUrl: './app-bar.component.html',
-  styleUrls: ['./app-bar.component.css']
+  styleUrls: ['./app-bar.component.css'],
 })
 export class AppBarComponent {
   @ViewChild(LoginModalComponent) loginModalComponent!: LoginModalComponent;
   loginModal: any;
 
+  router = inject(Router);
 
-  openLoginModal() {   
-   this.loginModalComponent.openModalhijo(); 
+  openLoginModal() {
+    this.loginModalComponent.openModalhijo();
+  }
+
+  cerrrarSesion() {
+    if (localStorage.getItem('miToken')) {
+      localStorage.removeItem('miToken');
+      this.router.navigate(['/pages']);
+    }
   }
 }
